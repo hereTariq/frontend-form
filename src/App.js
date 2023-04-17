ad89a1e4f70f1b5571c0e88105292463fd7899f7
Alfiya <alfiya96960@gmail.com>
2023-04-17 12:05:58 +0530

    minor changes

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
-import React, { useState } from 'react';
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import React, { useState } from "react";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 
-import LandingPage from './components/pages/LandingPage';
-import LoginPage from './components/pages/LoginPage';
-import RegisterPage from './components/pages/RegisterPage';
-import ForgetPasswordPage from './components/pages/ForgetPasswordPage';
-import FormPage from './components/pages/FormPage';
-import ResponseForm from './components/pages/ResponseForm';
+import LandingPage from "./components/pages/LandingPage";
+import LoginPage from "./components/pages/LoginPage";
+import RegisterPage from "./components/pages/RegisterPage";
+import ForgetPasswordPage from "./components/pages/ForgetPasswordPage";
+import FormPage from "./components/pages/FormPage";
+import ResponseForm from "./components/pages/ResponseForm";
 
-import './App.css';
+import "./App.css";
 
 export default function App() {
     const [user, setUser] = useState(null);
@@ -33,34 +33,34 @@ export default function App() {
                     element={<ResponseForm formData={formData} />}
                 />
             </Routes>
-            <Footer />
+            {/* <Footer /> */}
         </BrowserRouter>
     );
 }
 
-const Footer = () => {
-    return (
-        <p className="text-center" style={FooterStyle}>
-            Designed & coded by Alfiya
-            {/* <a
-                href="https://izemspot.netlify.com"
-                target="_blank"
-                rel="noopener noreferrer"
-            >
-                Alfiya
-            </a> */}
-        </p>
-    );
-};
+// const Footer = () => {
+//     return (
+//         <p className="text-center" style={FooterStyle}>
+//             Designed & coded by Alfiya
+//             {/* <a
+//                 href="https://izemspot.netlify.com"
+//                 target="_blank"
+//                 rel="noopener noreferrer"
+//             >
+//                 Alfiya
+//             </a> */}
+//         </p>
+//     );
+// };
 
-const FooterStyle = {
-    background: '#222',
-    fontSize: '.8rem',
-    color: '#fff',
-    position: 'absolute',
-    bottom: 0,
-    padding: '1rem',
-    margin: 0,
-    width: '100%',
-    opacity: '.5',
-};
+// const FooterStyle = {
+//     background: "#222",
+//     fontSize: ".8rem",
+//     color: "#fff",
+//     position: "absolute",
+//     bottom: 0,
+//     padding: "1rem",
+//     margin: 0,
+//     width: "100%",
+//     opacity: ".5",
+// };
